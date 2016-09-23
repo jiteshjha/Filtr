0219b8dab2d76263e854dbf559ae970352c33bb1
jiteshjha <jiteshjha96@gmail.com>
2016-09-23 23:02:40 +0530

    Sign up & Login activity.

--- app/src/main/java/com/jitesh/filtr/MainActivity.java
+++ app/src/main/java/com/jitesh/filtr/MainActivity.java
@@ -1,11 +1,17 @@
 package com.jitesh.filtr;
 
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.graphics.drawable.BitmapDrawable;
 import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.StaggeredGridLayoutManager;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
@@ -15,6 +21,8 @@ import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    Uri saveUri;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -22,7 +30,8 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Uri uri = Uri.parse(intent.getStringExtra("imageUri"));
-	String imageLink = intent.getStringExtra("imageUri");
+        saveUri = uri;
+	    String imageLink = intent.getStringExtra("imageUri");
         ImageView imageView = (ImageView)findViewById(R.id.imageView);
 
         imageView.setImageURI(uri);
@@ -38,7 +47,32 @@ public class MainActivity extends AppCompatActivity {
         ImageAdapter imageAdapter = new ImageAdapter(MainActivity.this, listFilters, imageLink);
         recyclerView.setAdapter(imageAdapter);
         recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL));
+    }
 
+    public void saveAction(View v) {
+        ImageView imageView = (ImageView)findViewById(R.id.imageView);
+        Intent intent = new Intent(getApplicationContext(), SaveActivity.class);
+        intent.putExtra("imageUri", saveUri.toString());
+        startActivity(intent);
+    }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.menu_settings:
+
+                return true;
+            case R.id.menu_about:
+                return true;
+            default:
+                return super.onOptionsItemSelected(item);
+        }
     }
 }
