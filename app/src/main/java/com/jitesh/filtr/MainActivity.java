9c595d3653bf6937fe8bf2dbc10686a6acd3558d
jiteshjha <jiteshjha96@gmail.com>
2016-09-23 18:43:34 +0530

    Added Recycler View for MainActivity

--- app/src/main/java/com/jitesh/filtr/MainActivity.java
+++ app/src/main/java/com/jitesh/filtr/MainActivity.java
@@ -5,10 +5,13 @@ import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.StaggeredGridLayoutManager;
 import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.ListAdapter;
 import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,14 +22,22 @@ public class MainActivity extends AppCompatActivity {
 
         Intent intent = getIntent();
         Uri uri = Uri.parse(intent.getStringExtra("imageUri"));
+	String imageLink = intent.getStringExtra("imageUri");
         ImageView imageView = (ImageView)findViewById(R.id.imageView);
 
         imageView.setImageURI(uri);
 
-        String[] someString= {"Filter 1", "Filter 1", "Filter 1", "Filter 1", "Filter 1", "Filter 1", "Filter 1"};
-        ListView listView= (ListView)findViewById(R.id.listView);
-        ListAdapter adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, someString);
-        listView.setAdapter(adapter);
+        String[] someString = {"Filter 1", "Filter 2", "Filter 3", "Filter 4", "Filter 5", "Filter 6", "Filter 7"};
+        ArrayList<Filter> listFilters = new ArrayList<>();
+        for (String x : someString) {
+            Filter filter = new Filter();
+            filter.setFilterName(x);
+            listFilters.add(filter);
+        }
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerViewMain);
+        ImageAdapter imageAdapter = new ImageAdapter(MainActivity.this, listFilters, imageLink);
+        recyclerView.setAdapter(imageAdapter);
+        recyclerView.setLayoutManager(new StaggeredGridLayoutManager(1, StaggeredGridLayoutManager.HORIZONTAL));
 
 
     }
